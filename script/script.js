64574af9ad86dc2ac8957aec81610b813331fb1d
Willy Mondzelewski <willymondz@gmail.com>
2022-10-10 17:13:49 -0300

    Primeiro commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('#scroll-menu a[href^="#"]');
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
+
+function initAnimaScroll() {
+  const sections = document.querySelectorAll('.js-scroll')
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.8
+
+    function scrollactive(){
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top
+        const isSectionVisible = (sectionTop - windowMetade) < 0
+        if(isSectionVisible) {
+          section.classList.add('ativo')
+        }
+      })
+    }
+    window.addEventListener('scroll', scrollactive)
+    scrollactive()
+  }
+}
+initAnimaScroll()
